4bde5eaccae7dc91264844de5da66086dbbc0582
gkluhana <gkluhana@gmail.com>
2019-03-01 19:34:48 -0500

    Deleting MATLAB folder

--- MATLAB/ib_matlab_2D/laplacian.m
+++
@@ -1,8 +0,0 @@
-function w=laplacian(u)
-global ixm ixp iym iyp h;
-w=(u(ixp,:,:)+u(ixm,:,:)+u(:,iyp,:)+u(:,iym,:)-4*u)/(h*h);
-
-% im,ixm, iym = [N,1:(N-1)] = circular version of i-1
-% ip, ixp , iym = [2:N,1]     = circular version of i+1
-% N  = number of points in each space direction
-
