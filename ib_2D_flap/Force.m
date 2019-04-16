6f22365924abd5b45c0d24fb3ca40fada0cd45b5
Gautam K. Luhana <gkluhana@gmail.com>
2019-04-16 12:43:53 -0400

    Switches in Force.m

--- ib_2D_flap/Force.m
+++ ib_2D_flap/Force.m
@@ -2,25 +2,31 @@ function [F,T]=Force(p,X,T)
 % global kp km dtheta K T;
 
 %Matrix for adding Deviation to flappers
+Free = 1;
+leaderFree = 1;
+followerFree=1;
 
-leader   = 1:p.Nb;
-follower = leader + p.Nb;
-
-E = zeros(size(X,1),size(X,2),p.num_flappers);
-E(leader,  1,1)  = 1;
-E(follower,1,2)  = 1;
-
+if Free
+	leader   = 1:p.Nb;
+	follower = leader + p.Nb;
+	E = zeros(size(X,1),size(X,2),p.num_flappers);
+	E(leader,  1,1)  = 1;
+	E(follower,1,2)  = 1;
+end
 
 %F=K*(X(kp,:)+X(km,:)-2*X)/(dtheta*dtheta);
 
 %Compute Deviation with zero horizontal force for flappers
+if leaderFree
+	D(1)  = -(1/p.Nb)*sum(T(leader,  1) - X(leader,  1)); 
+	T = T+D(1)*E(:,:,1);
+end
 
-D(1)  = -(1/p.Nb)*sum(T(leader,  1) - X(leader,  1)); 
-D(2)  = -(1/p.Nb)*sum(T(follower,1) - X(follower,1));
+if followerFree 
+	D(2)  = -(1/p.Nb)*sum(T(follower,1) - X(follower,1));
+	T = T+D(2)*E(:,:,2);
+end
 
-%Compute new position of Target Points 
-T = T+D(1)*E(:,:,1);
-T = T+D(2)*E(:,:,2);
 
 %Compute Force
 F = -p.K*(X-T);
