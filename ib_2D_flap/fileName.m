6ea849b5d2cd81f4f3b97efa871af711b27f0124
Gautam K. Luhana <gkluhana@gmail.com>
2019-08-14 04:16:16 -0400

    Push from server

--- ib_2D_flap/fileName.m
+++ ib_2D_flap/fileName.m
@@ -1,6 +1,7 @@
 function name = filename(p)
 
 %Make a filename for the plots
-Node = '-hpc-';
+Node = '-crunchy-lateral';
+
 
 name = strcat('Sim',Node,'Tmax',num2str(p.tmax),'s','f',num2str(p.freq),'N',num2str(p.N),'mu',num2str(p.mu),'AC',num2str(p.AoverC),'gap',num2str(p.Tail2Head),'dt',num2str(p.dt))
